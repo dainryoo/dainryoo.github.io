43c49ffef0c62e3e50841fa09dc3b51efd32bbf5
dainryoo <dainryoo@gmail.com>
2018-09-18 19:18:01 -0400

    Minor styling changes

---
+++ case/isidGuide.js
@@ -0,0 +1,31 @@
+var isidGuide = [
+    {
+        title: "The task",
+        content: "<p>During the summer of 2018, I was given the opportunity to be a part of <b>IBM Design's Patterns internship</b>. I, along with 19 other designers from all over the country and all kinds of backgrounds, spent 10 weeks at the Design Studio in Austin, Texas.</p> <p>The interns were split into small groups and assigned a project for the summer. Our team, Team Code Blue, was made up of a visual designer, a UX designer, a UX researcher, and another front-end developer. While most other teams were assigned internal IBM projects, we were paired with the International Society of Infectious Diseases (ISID). ISID had reached out to IBM in hopes of developing a solution to <b>empower healthcare workers practicing infection prevention and control (IPC) in low- and middle-income countries (LMIC)</b>. ISID has a Guide to Infection Control in the Healthcare Setting, in both print and PDF form, but it isn't accessible and has problems reaching people in LMICs. ISID needed a prototype to present at the 2018 International Meeting on Emerging Diseases and Surveillance in order to get funding to develop the solution we designed.</p>"
+
+    },
+    {
+        title: "Oh boy that's a huge problem space",
+        content: "<p>We were thrown into a domain that none of us had any experience with. We spent a few days researching, scrambling to understand the world of IPC, the types of healthcare workers, and their unique pain points. Initially, we narrowed in on the community healthcare workers (CHW) of sub-Saharan Africa. These CHWs are personnel trusted by their communities, responsible for very \"on the field\" work and yet armed with less than ideal education and resources.</p> <p>We were blinded by our idealism, so drawn to the idea that we could design a solution to reach millions of people, especially the people in LMICs who needed help the most. The fact that these CHWs mostly carried feature phones and lived in areas with little to no Internet were not obstacles in our eyes but exciting challenges to overcome.</p> <p>Slowly we realized that this was not feasible. We had less than two months to interview CHWs in vastly different time zones and with unreliable cell signals; the developers had never done mobile development before, much less development for feature phones or for users with spotty Internet.</p>"
+    },
+    {
+        title: "Our solution",
+        content: "<p>And so we, a little reluctantly, let go of all the research (and the oddly emotional connection we had made with the CHW persona) and settled on something more realistic. <b>Our final idea was to design a mobile application that organized the ISID Guide into a more readable and searchable format, as well as supported localized content-creation by infection control specialists on a hopsital level.</b></p>"
+    },
+    {
+        title: "Preparing for Development (or, I reluctantly enter the world of React)",
+        content: "<p>I had come in as a front-end developer, but the intensely collaborative nature of the internship let me explore the other disciplines and work closely alongside the designers on our team. But after enjoying a few weeks of design thinking, brainstorming, and researching, it was time I returned to the world of development.</p> <p>We settled on using <b>React Native</b> for our platform agnostic solution; we barely had time to make one app, much less two different versions for Android and iOS. I had never worked with React Native (not even React!) before, so I spent about a week bodyslamming myself into videos and textbooks and online tutorials.</p>"
+    },
+    {
+        title: "Obstacles",
+        content: "-"
+    },
+    {
+        title: "Development (or, I actually find myself enjoying mobile development)",
+        content: "-"
+    },
+    {
+        title: "And the summer ends.",
+        content: "-"
+    }
+];
---
+++ case/portfolioItems.js
@@ -0,0 +1,44 @@
+var projects = [
+    {
+        title: "ISID Guide",
+        link: "casestudy-isid-guide",
+        linkType: "Case Study",
+        date: "Summer 2018",
+        summary: "During my summer at IBM Design's Patterns Internship, my team collaborated with ISID to turn their infection prevention and control <a href=\"http://www.isid.org/education/guide-to-infection-control-in-the-healthcare-setting/\" target=\"_blank\">guide</a> into a mobile solution."
+    },
+    {
+        title: "#mood",
+        link: "https://dainryoo.github.io/color-me",
+        linkType: "Live",
+        date: "Feb 2018",
+        summary: "Created at Pearl Hacks 2018, #mood asks you how you're feeling and shows you the hexadecimal color representation of your current emotion. Record and visualize the progression of your mood with a color timeline."
+    },
+    {
+        title: "Atlanta Farms, Gardens, and Orchards",
+        link: "http://farm.stephanieg.xyz",
+        linkType: "Live",
+        date: "Spring 2018",
+        summary: "A semester long team project involving data retrieval, storage, and editing, in addition to different pages for different user types. The website was built with PHP and MySQL, and I worked on database manipulation and the front-end."
+    },
+    {
+        title: "Social Music",
+        link: "casestudy-social-music",
+        linkType: "Case Study",
+        date: "Spring 2017",
+        summary: "A mobile app that builds unique playlists by automatically queuing up songs from playlists of landmarks and passersby the user walks past. The project began with user research and ended with conducting tests of our InVision prototype."
+    },
+    {
+        title: "Lukewarm Greeting Cards",
+        link: "https://dainryoo.github.io/lukewarm",
+        linkType: "Live",
+        date: "Feb 2017",
+        summary: "Created at Pearl Hacks 2017, Lukewarm Greeting Cards is a responsive website that generates unique half-hearted messages for various holidays/occasions."
+    },
+    {
+        title: "A chicken considers crossing the road",
+        link: "https://youtu.be/5AyqufgUVF0",
+        linkType: "Video",
+        date: "Spring 2017",
+        summary: "A 3D animation about a cautious chicken, done in Python with Processing's basic shapes and matrix transformations."
+    }
+];
